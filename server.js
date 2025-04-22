ffb3abe9c2ac4cf7ab53c385d2323bfdfffa972e
jeffbarross <jezera84@gmail.com>
2025-04-21 22:40:27 -0300

    corrigido uso da porta para Render

--- server.js
+++ server.js
@@ -41,4 +41,6 @@ app.post('/api/bloquear', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
